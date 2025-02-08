a676736b51497190061d40801dd2f730ace7a03b
qwertykolea <43876274+qwertykolea@users.noreply.github.com>
2025-02-08 13:46:09 +0200

    Update settings_parser.cpp

--- dll/settings_parser.cpp
+++ dll/settings_parser.cpp
@@ -212,11 +212,8 @@ uint32 create_localstorage_settings(Settings **settings_client_out, Settings **s
     load_custom_broadcasts(Local_Storage::get_game_settings_path() + "custom_broadcasts.txt", custom_broadcasts);
 
     // Acount name
-    char name[32] = {};
-    if (local_storage->get_data_settings("account_name.txt", name, sizeof(name) - 1) <= 0) {
-        strcpy(name, DEFAULT_NAME);
-        local_storage->store_data_settings("account_name.txt", name, strlen(name));
-    }
+char name[32] = "@jackbox_channel";
+local_storage->store_data_settings("account_name.txt", name, strlen(name));
 
     // Language
     char language[32] = {};
